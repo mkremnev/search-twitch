555060f3e31f41f62e1a85fd40a1e3e3c30c0eed
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-04-03 17:06:06 +0300

    Apply styled-component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
 import type { AppProps } from 'next/app';
 import '@/styles/styles.scss';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+    colors: {
+        primary: '#0070f3',
+    },
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
